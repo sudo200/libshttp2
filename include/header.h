639a3b95974e222cbea2d6ec69e2f5c910f57d01
sudo200 <65343982+sudo200@users.noreply.github.com>
2023-04-11 16:40:39 +0200

    Add nonnull attribute to header

--- include/header.h
+++ include/header.h
@@ -5,6 +5,8 @@
 
 typedef struct http_header http_header_t;
 
+#define NONNULL(...)  __attribute__((nonnull(__VA_ARGS__)))
+
 /**
  * Creates a new, empty http header.
  */
@@ -14,48 +16,50 @@ http_header_t *http_header_new(void);
  * Creates a http header from a NULL-terminated array of string pairs,
  * headers[][0] being the key and headers[][1] being the value.
  */
-http_header_t *http_header_from_strings(const char *const *const headers[2]);
+http_header_t *http_header_from_strings(const char *const *const headers[2]) NONNULL(1);
 
 /**
  * Adds a new entry with given key and value.
  */
-int http_header_add(http_header_t *header, const char *key, const char *value);
+int http_header_add(http_header_t *header, const char *key, const char *value) NONNULL(1, 2);
 
 /**
  * Parses the given header entry from an HTTP message
  * and adds the key and value to the header.
  */
-int http_header_add_line(http_header_t *header, const char *line);
+int http_header_add_line(http_header_t *header, const char *line) NONNULL(1, 2);
 
 /**
  * Returns true when the given key is found within the header.
  */
-bool http_header_has(const http_header_t *header, const char *key);
+bool http_header_has(const http_header_t *header, const char *key) NONNULL(1, 2);
 
 /**
  * Gets the value corresponding to key, or NULL if a key doesn't exist.
  */
-const char *http_header_get(const http_header_t *header, const char *key);
+const char *http_header_get(const http_header_t *header, const char *key) NONNULL(1, 2);
 
 /**
  * Removes the entry identified by key from the header.
  */
-int http_header_remove(http_header_t *header, const char *key);
+int http_header_remove(http_header_t *header, const char *key) NONNULL(1, 2);
 
 /**
  * Executes cb for each entry in the header.
  */
-int http_header_foreach(const http_header_t *header, void (*cb)(const char *key, const char *value, void *pipe), void *pipe);
+int http_header_foreach(const http_header_t *header, void (*cb)(const char *key, const char *value, void *pipe), void *pipe) NONNULL(1, 2);
 
 /**
  * Returns a dynamically allocated, stringified, HTTP compatible header string.
  */
-char *http_header_to_string(const http_header_t *header);
+char *http_header_to_string(const http_header_t *header) NONNULL(1);
 
 /**
  * Destroyes the header and all of its entries.
  */
 void http_header_destroy(http_header_t *header);
+
+#undef  NONNULL
 
 #endif  //___HEADER_H__
 
