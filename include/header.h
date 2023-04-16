379fa5d7146d556cc394533dd87121426e79ce3b
sudo200 <65343982+sudo200@users.noreply.github.com>
2023-04-16 20:38:17 +0200

    Implement all methods of http_header

--- include/header.h
+++ include/header.h
@@ -11,57 +11,63 @@ typedef struct {
 
 #define NONNULL(...)  __attribute__((nonnull(__VA_ARGS__)))
 
+typedef struct {
+
 /**
  * Creates a new, empty http header.
  */
-http_header_t *http_header_new(void);
+http_header_t *(*init)(void);
 
 /**
  * Creates a http header from a NULL-terminated array of string pairs,
  * headers[][0] being the key and headers[][1] being the value.
  */
-http_header_t *http_header_from_strings(const http_header_entry_t *entries) NONNULL(1);
+http_header_t *(*from_strings)(const http_header_entry_t *entries) NONNULL(1);
 
 /**
  * Adds a new entry with given key and value.
  */
-int http_header_add(http_header_t *header, const char *key, const char *value) NONNULL(1, 2);
+int (*add)(http_header_t *header, const char *key, const char *value) NONNULL(1, 2);
 
 /**
  * Parses the given header entry from an HTTP message
  * and adds the key and value to the header.
  */
-int http_header_add_line(http_header_t *header, const char *line) NONNULL(1, 2);
+int (*add_line)(http_header_t *header, const char *line) NONNULL(1, 2);
 
 /**
  * Returns true when the given key is found within the header.
  */
-bool http_header_has(const http_header_t *header, const char *key) NONNULL(1, 2);
+bool (*has)(const http_header_t *header, const char *key) NONNULL(1, 2);
 
 /**
  * Gets the value corresponding to key, or NULL if a key doesn't exist.
  */
-const char *http_header_get(const http_header_t *header, const char *key) NONNULL(1, 2);
+const char *(*get)(const http_header_t *header, const char *key) NONNULL(1, 2);
 
 /**
  * Removes the entry identified by key from the header.
  */
-int http_header_remove(http_header_t *header, const char *key) NONNULL(1, 2);
+int (*remove)(http_header_t *header, const char *key) NONNULL(1, 2);
 
 /**
  * Executes cb for each entry in the header.
  */
-int http_header_foreach(const http_header_t *header, void (*cb)(const char *key, const char *value, void *pipe), void *pipe) NONNULL(1, 2);
+int (*foreach)(const http_header_t *header, void (*cb)(const char *key, const char *value, void *pipe), void *pipe) NONNULL(1, 2);
 
 /**
  * Returns a dynamically allocated, stringified, HTTP compatible header string.
  */
-char *http_header_to_string(const http_header_t *header) NONNULL(1);
+char *(*to_string)(const http_header_t *header) NONNULL(1);
 
 /**
  * Destroyes the header and all of its entries.
  */
-void http_header_destroy(http_header_t *header);
+void (*destroy)(http_header_t *header);
+
+} http_header_methods_t;
+
+extern const http_header_methods_t http_header;
 
 #undef  NONNULL
 
--- src/header.c
+++ src/header.c
@@ -4,8 +4,7 @@
 #include <sutil/dmem.h>
 #include <sutil/hash.h>
 #include <sutil/hashmap.h>
-#include <sutil/set.h>
-#include <sutil/treeset.h>
+#include <sutil/mstring.h>
 
 #include "header.h"
 
@@ -18,12 +17,12 @@ typedef struct {
 
 struct http_header {
   hashmap_t *map;
-  set_t *refs;
 };
 
 // Private functions
-static void ref_destr(void *ref, size_t sz __attribute__((unused)), void *pipe __attribute__((unused))) {
-  ufree(ref);
+static void ref_destr(void *key, size_t sz __attribute__((unused)), void *value, void *pipe __attribute__((unused))) {
+  ufree(key);
+  ufree(value);
 }
 
 static void helper_cb(void *key, size_t keysz __attribute__((unused)), void *value, void *pipe) {
@@ -31,8 +30,19 @@ static void helper_cb(void *key, size_t keysz __attribute__((unused)), void *val
   data->real_cb((const char *)key, (const char *)value, data->real_pipe);
 }
 
+static void to_string_cb(void *__key, size_t ___keysz __attribute__((unused)), void *__value, void *__pipe) {
+  const char *key = (const char *)__key;
+  const char *value = (const char *)__value;
+  char **str = (char **)__pipe;
+
+  char *line;
+  msprintf(&line, "%s: %s\r\n", key, value);
+  mstrcat(str, line);
+  ufree(line);
+}
+
 // Public functions
-http_header_t *http_header_new(void) {
+static http_header_t *http_header_init(void) {
   http_header_t *header = (http_header_t *)ualloc(sizeof(*header));
   if(header == NULL) {
     errno = ENOMEM;
@@ -41,19 +51,17 @@ http_header_t *http_header_new(void) {
 
   if((header->map = hashmap_new_prealloc(fnv1a, 5)) == NULL)
     return NULL;
-  if((header->refs = (set_t *)treeset_new()) == NULL)
-    return NULL;
 
   return header;
 }
 
-http_header_t *http_header_from_strings(const http_header_entry_t *entries) {
-  http_header_t *header = http_header_new();
+static http_header_t *http_header_from_strings(const http_header_entry_t *entries) {
+  http_header_t *header = http_header.init();
   if(header == NULL)
     return NULL;
   
   while (entries->key && entries->value) {
-    if(http_header_add(header, entries->key, entries->value) < 0)
+    if(http_header.add(header, entries->key, entries->value) < 0)
       goto _error;
     entries++;
   }
@@ -62,13 +70,13 @@ http_header_t *http_header_from_strings(const http_header_entry_t *entries) {
 
   _error: {
     errno_t tmp = errno;
-    http_header_destroy(header);
+    http_header.destroy(header);
     errno = tmp;
     return NULL;
   }
 }
 
-int http_header_add(http_header_t *header, const char *key, const char *value) {
+static int http_header_add(http_header_t *header, const char *key, const char *value) {
   const size_t keysz = strlen(key), valuesz = strlen(value);
   char *_key = NULL, *_value = NULL;
  
@@ -76,12 +84,6 @@ int http_header_add(http_header_t *header, const char *key, const char *value) {
     goto _error;
 
   if((_value = ualloc(valuesz + sizeof(*_value))) == NULL)
-    goto _error;
-
-  if(set_add(header->refs, _key, keysz) < 0)
-    goto _error;
-
-  if(set_add(header->refs, _value, valuesz) < 0)
     goto _error;
 
   memcpy(_key, key, keysz + sizeof(*_key));
@@ -95,8 +97,6 @@ int http_header_add(http_header_t *header, const char *key, const char *value) {
 
 _error: {
     errno_t tmp = errno;
-    set_remove(header->refs, _key, keysz);
-    set_remove(header->refs, _value, valuesz);
     ufree(_key);
     ufree(_value);
     errno = tmp;
@@ -104,7 +104,49 @@ _error: {
   }
 }
 
-bool http_header_has(const http_header_t *header, const char *key) {
+static int http_header_add_line(http_header_t *header, const char *__line) {
+  char *line, *key = NULL, *value = NULL;
+  if(msprintf(&line, "%s", __line) < 0)
+    return -1;
+
+  char *v = strchr(line, ':');
+  if(v == NULL) {
+    errno = EINVAL;
+    goto _error;
+  }
+
+  *v++ = '\0';
+  
+  if(msprintf(&key, "%s", line) < 0)
+    goto _error;
+  if(msprintf(&value, "%s", v) < 0)
+    goto _error;
+
+  ufree(line);
+  line = NULL;
+
+  if(strtrim(&key) != 0)
+    goto _error;
+  if(strtrim(&value) != 0)
+    goto _error;
+
+  if(http_header.add(header, key, value) < 0)
+    goto _error;
+
+  ufree(key);
+  ufree(value);
+
+  return 0;
+
+_error: {
+    ufree(line);
+    ufree(key);
+    ufree(value);
+    return -1;
+  }
+}
+
+static bool http_header_has(const http_header_t *header, const char *key) {
   return hashmap_contains_key(header->map, stra(key));
 }
 
@@ -112,21 +154,19 @@ const char *http_header_get(const http_header_t *header, const char *key) {
   return (const char *)hashmap_get(header->map, stra(key));
 }
 
-int http_header_remove(http_header_t *header, const char *key) {
+static int http_header_remove(http_header_t *header, const char *key) {
   errno = 0;
   void *value = hashmap_remove(header->map, stra(key));
   if(value == NULL && errno != 0)
     return -1;
   void *_key = hashmap_key(header->map, stra(key));
 
-  set_remove(header->refs, stra(key));
-  set_remove(header->refs, stra(value));
   ufree(_key);
   ufree(value);
   return 0;
 }
 
-int http_header_foreach(const http_header_t *header, void (*cb)(const char *, const char *, void *), void *pipe) {
+static int http_header_foreach(const http_header_t *header, void (*cb)(const char *, const char *, void *), void *pipe) {
   helper_cb_data_t data = {
     .real_cb = cb,
     .real_pipe = pipe,
@@ -134,16 +174,38 @@ int http_header_foreach(const http_header_t *header, void (*cb)(const char *, co
   return hashmap_foreach(header->map, helper_cb, &data);
 }
 
-void http_header_destroy(http_header_t *header) {
+static char *http_header_to_string(const http_header_t *header) {
+  char *str = NULL;
+  if(hashmap_foreach(header->map, to_string_cb, &str) < 0) {
+    ufree(str);
+    return NULL;
+  }
+  return str;
+}
+
+static void http_header_destroy(http_header_t *header) {
   if(header == NULL)
     return;
 
-  set_foreach(header->refs, ref_destr, NULL);
-  
+  hashmap_foreach(header->map, ref_destr, NULL);
   hashmap_destroy(header->map);
-  set_destroy(header->refs);
   ufree(header);
 }
+
+
+
+const http_header_methods_t http_header = {
+  http_header_init,
+  http_header_from_strings,
+  http_header_add,
+  http_header_add_line,
+  http_header_has,
+  http_header_get,
+  http_header_remove,
+  http_header_foreach,
+  http_header_to_string,
+  http_header_destroy
+};
 
 #undef stra
 
